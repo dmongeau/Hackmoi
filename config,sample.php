8841251f88d59b3e04b57fdb57e9ca6e3fa501e5
David Mongeau-Petitpas <dmongeau_petitpas@ecoutez.ca>
2011-10-31 23:38:19 -0400

    Added config sample

---
+++ config,sample.php
@@ -0,0 +1,13 @@
+<?php
+
+$config = array(
+	'adapter' => 'pdo_mysql',
+	'config' => array(
+		'host' => 'localhost',
+		'username' => 'votes',
+		'password' => 'temp1234',
+		'dbname' => 'votes'
+	)
+);
+
+return $config;
